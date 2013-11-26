93da41494550358eef9ec24f9e555d7e05485dda
jeliser <jeliser@gmail.com>
2013-11-26 15:02:18 -0600

    Update the bootstrap script

--- bootstrap_vim.sh
+++ bootstrap_vim.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env sh
 
 # Put this in a script like:  install_vim_from_source
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
-tar xvjf vim-7.4.tar.bz2
-cd vim74
+#wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
+#tar xvjf vim-7.4.tar.bz2
+#cd vim74
 # Determine is yum or apt-get should be called
-sudo apt-get install ncurses-devel
-./configure --enable-pythoninterp --enable-multibyte --with-tlib=ncurses
-make -j 10; sudo make install
-cd ../
+#sudo apt-get install ncurses-devel
+#./configure --enable-pythoninterp --enable-multibyte --with-tlib=ncurses
+#./configure --enable-pythoninterp --enable-multibyte --with-tlib=tinfo
+#make -j 10; sudo make install
+#cd ../
 
 
 if [ ! -e $HOME/.vim/bundle/vundle ]; then
   echo "Installing Vundle"
-  git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+  git clone https://github.com/gmarik/vundle $HOME/.vim/bundle/vundle
 fi
 
 echo "update/install plugins using Vundle"
 system_shell=$SHELL
 export SHELL="/bin/sh"
-vim -u `pwd`/.vimrc.bundles +BundleInstall! +BundleClean +qall
+vim -u $HOME/.vimrc.bundles +BundleInstall! +BundleClean +qall
 export SHELL=$system_shell
 
 # Install dependacies for YouCompleteMe

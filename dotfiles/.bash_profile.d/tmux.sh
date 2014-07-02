8aa94daa49dda75c248aba82a84cba567415a474
Josh <jeliser@gmail.com>
2014-07-02 12:31:46 -0500

    Moved where the auto-bash tmux script is executed from.

---
+++ dotfiles/.bash_profile.d/tmux.sh
@@ -0,0 +1,22 @@
+
+SESSIONNAME="Base_View"
+tmux has-session -t $SESSIONNAME >> /dev/null
+
+if [ $? -ne 0 ]
+  then
+    # new session with name $SESSIONNAME and window 0 named "base"
+    tmux new-session -s $SESSIONNAME -n "Main" -d
+    tmux split-window -t $SESSIONNAME:1 -v
+    tmux select-pane -t 1
+    tmux split-window -t $SESSIONNAME:1 -h
+    tmux send-keys 'htop' 'C-m'
+    # Potentially show adb connections or something
+    #tmux split-window -t $SESSIONNAME:1 -v
+    #tmux send-keys 'mkdir ./bin' 'C-m'
+
+    tmux select-pane -t 3
+    tmux resize-pane -U 8
+    tmux attach -t $SESSIONNAME
+fi
+
+

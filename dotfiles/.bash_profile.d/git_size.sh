8b1dbd5435795e144d1c60726c4bdbf349e48e0d
Josh <jeliser@gmail.com>
2014-09-18 12:36:27 -0500

    Fixed up the scripts to go up the tree to find git history and added the date to blob read out.

--- dotfiles/.bash_profile.d/git_size.sh
+++ dotfiles/.bash_profile.d/git_size.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 git_size() {
   # http://stubbisms.wordpress.com/2009/07/10/git-script-to-show-largest-pack-objects-and-trim-your-waist-line/
-   
+  
+  # Search up the tree for the .git directory
+  path=""
+  i=0
+  while [ $i -lt 20 ] && [ `find $path -type d | grep .git | wc -l` -eq 0 ]; do
+    path+="../"
+    let i+=1
+  done
+
   # set the internal field spereator to line break, so that we can iterate easily over the verify-pack output
   IFS=$'\n';
    
   # list all objects including their size, sort by size, take top 10
-  objects=`git verify-pack -v .git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n $1 | tr -s ' '`
+  objects=`git verify-pack -v $path.git/objects/pack/pack-*.idx | grep -v chain | sort -k3nr | head -n $1 | tr -s ' '`
   echo "All sizes are in kB's. The pack column is the size of the object, compressed, inside the pack file."
    
   output="size,pack,SHA,location"

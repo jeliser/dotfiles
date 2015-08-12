906a14d18dd9d5c4a0c9573afe8b56d4f53ce070
Josh <jeliser@gmail.com>
2015-08-12 14:56:17 -0500

    Added a bootstrap for setting the autocomplete when using git.

---
+++ bootstrap_git.sh
@@ -0,0 +1,9 @@
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+
+echo "" >> ~/.bashrc.local
+echo "if [ -f ~/.git-completion.bash ]; then" >> ~/.bashrc.local
+echo "  . ~/.git-completion.bash" >> ~/.bashrc.local
+echo "fi" >> ~/.bashrc.local
+echo "" >> ~/.bashrc.local
+

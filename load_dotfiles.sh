0989a0a46beda2a8dbaa4ea8f04dc59f4041c0dd
jeliser <jeliser@gmail.com>
2016-12-07 11:51:46 -0600

    Updating to the latest version of tmux and fixing the .gitconfig inital setup.

--- load_dotfiles.sh
+++ load_dotfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 dotfiles -s -R `pwd`/dotfiles --force
-mv ~/.gitconfig.public ~/.gitconfig
+if [ -f "$HOME/.gitconfig.public" ] && ! [ -f "$HOME/.gitconfig" ] ; then
+  cp $HOME/.gitconfig.public $HOME/.gitconfig
+fi
 
 mkdir -p ~/.vim/.undodir;
 mkdir -p ~/.vim/backup;
